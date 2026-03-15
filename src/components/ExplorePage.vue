81b86743baebdda9cd6801b8f47faba69d9ac6e4
Ifthikar20 <ifthicaralikhan@gmail.com>
2026-03-14 19:12:53 -0500

    fix: remove like button from brand cards

--- src/components/ExplorePage.vue
+++ src/components/ExplorePage.vue
@@ -120,20 +120,6 @@
                 <span class="brand-name">{{ brand.name }}</span>
                 <span class="brand-category">{{ brand.category }}</span>
               </div>
-              <button
-                class="like-btn"
-                :class="{ liked: brand.is_liked }"
-                @click.stop="toggleLike(brand)"
-                :title="brand.is_liked ? 'Unlike' : 'Like'"
-              >
-                <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-                  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
-                    2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09
-                    C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5
-                    c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                </svg>
-                <span class="like-count">{{ brand.likes_count }}</span>
-              </button>
             </div>
           </div>
         </section>
